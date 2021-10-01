6ec58bdbca1744a67b7c3a10a8f1ce964c6e44ee
Olivier Lischer <olivier.lischer@liolin.ch>
2021-10-01 14:58:45 +0200

    Add websockets client library

--- app/src/main/java/ch/ost/mge/mini/lanchat/SettingsActivity.kt
+++ app/src/main/java/ch/ost/mge/mini/lanchat/SettingsActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import ch.ost.mge.mini.lanchat.SettingsStore;
 
 class SettingsActivity : AppCompatActivity() {
+    private val store = SettingsStore()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val switchToHomeActivity = Intent(this, MainActivity::class.java)
         val btnBack = findViewById<Button>(R.id.btnSettingsBackToHome)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        val txtServerAddress = findViewById<EditText>(R.id.txtServerAddress)
+        val txtUsername = findViewById<EditText>(R.id.txtUsername)
+
         btnBack.setOnClickListener { startActivity(switchToHomeActivity) }
+        btnSave.setOnClickListener {
+            store.serverAddress = txtServerAddress.text.toString()
+            store.username = txtUsername.text.toString()
+        }
     }
 }
---
+++ app/src/main/java/ch/ost/mge/mini/lanchat/SettingsStore.kt
@@ -0,0 +1,6 @@
+package ch.ost.mge.mini.lanchat
+
+class SettingsStore {
+    var serverAddress: String = ""
+    var username: String = ""
+}
