36a1550a189cbc9a4ddbe0e579860c902eb7806d
Olivier Lischer <olivier.lischer@liolin.ch>
2021-10-01 11:22:43 +0200

    Add ChatActivity

--- app/src/main/java/ch/ost/mge/mini/lanchat/MainActivity.kt
+++ app/src/main/java/ch/ost/mge/mini/lanchat/MainActivity.kt
@@ -1,11 +1,17 @@
 package ch.ost.mge.mini.lanchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchToChatActivityIntend = Intent(this, ChatActivity::class.java)
+        val chatButton = findViewById<Button>(R.id.btnGoToChat);
+        chatButton.setOnClickListener { startActivity(switchToChatActivityIntend) }
     }
 }
