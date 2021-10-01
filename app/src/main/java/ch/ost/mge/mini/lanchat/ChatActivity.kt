421a9ab35e3346b5a7860b9dcf5873be50fa0874
Olivier Lischer <olivier.lischer@liolin.ch>
2021-10-01 11:26:39 +0200

    Add Back button in ChatAcitivty View

--- app/src/main/java/ch/ost/mge/mini/lanchat/ChatActivity.kt
+++ app/src/main/java/ch/ost/mge/mini/lanchat/ChatActivity.kt
@@ -1,11 +1,18 @@
 package ch.ost.mge.mini.lanchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+
+        val goToHomeIntend = Intent(this, MainActivity::class.java)
+        val btnBack = findViewById<Button>(R.id.btnBackToHome)
+        btnBack.setOnClickListener { startActivity(goToHomeIntend) }
     }
 }
