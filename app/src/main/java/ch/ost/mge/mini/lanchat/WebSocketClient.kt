89dd23c9b693c0898e9baa2b4c0f9284f9a2660c
Olivier Lischer <olivier.lischer@liolin.ch>
2021-10-01 15:59:51 +0200

    Add simple WebSocketClient

---
+++ app/src/main/java/ch/ost/mge/mini/lanchat/WebSocketClient.kt
@@ -0,0 +1,26 @@
+package ch.ost.mge.mini.lanchat
+
+import android.util.Log
+import java.lang.Exception
+import java.net.URI
+
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+
+class WebSocketClient(uri: URI) : WebSocketClient(uri) {
+    override fun onOpen(handshakedata: ServerHandshake?) {
+        Log.d("WebSocketClient", "Connection opened")
+    }
+
+    override fun onMessage(message: String?) {
+        Log.d("WebSocketClient", "Message received: $message")
+    }
+
+    override fun onClose(code: Int, reason: String?, remote: Boolean) {
+        Log.d("WebSocketClient", "Connection closed")
+    }
+
+    override fun onError(ex: Exception?) {
+        Log.d("WebSocketClient", "Oh no, something went wrong: ${ex.let { it.toString() }}")
+    }
+}
